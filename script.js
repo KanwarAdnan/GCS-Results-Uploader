a1908974847f6117305f2be0a74b85bb1d6cebcf
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2023-09-10 01:04:53 +0500

    Enhanced features

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 const fileInput = document.getElementById('fileInput');
 const fileTableBody = document.querySelector('#fileTable tbody');
 const uploadButton = document.getElementById('uploadButton');
-const clearButton = document.getElementById('clearButton');
 const fileTable = document.getElementById('fileTable');
 const uploadStatusMessage = document.getElementById('uploadStatusMessage');
 
 let uploadedFiles = 0;
 let failedFiles = 0;
+let selectedFiles = [];
+
+uploadButton.style.display = 'none';
 
 uploadButton.addEventListener('click', () => {
   uploadStatusMessage.textContent = '';
   toggleFileTable();
-  handleFileUpload();
+  clearFileTable();
+  handleFileUpload(selectedFiles);
 });
 
 function toggleFileTable() {
-  fileTable.style.display = fileInput.files.length > 0 ? 'table' : 'none';
+  fileTable.style.display = selectedFiles.length > 0 ? 'table' : 'none';
+  
+  if (selectedFiles.length > 0) {
+    showUploadButton();
+  } else {
+    uploadStatusMessage.textContent = 'No files selected.';
+    uploadButton.style.display = 'none';
+  }
 }
 
-fileInput.addEventListener('change', () => {
-  updateStatusMessageOnFileChange();
-});
+
+function showUploadButton() {
+  uploadButton.style.display = 'inline'; 
+}
 
 function clearFileInput() {
   fileInput.value = '';
@@ -28,22 +39,41 @@ function clearFileInput() {
   fileTable.style.display = 'none';
   uploadedFiles = 0;
   failedFiles = 0;
+  selectedFiles = [];
+  uploadButton.style.display = 'none'; 
 }
 
-function formatFileSize(bytes) {
-  if (bytes === 0) return '0 Bytes';
-  const k = 1024;
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
-}
-
-async function handleFileUpload() {
+fileInput.addEventListener('change', () => {
+  fileTableBody.innerHTML = '';
   const files = fileInput.files;
-  const totalFiles = files.length;
+  for (const file of files) {
+    if (!file.name.endsWith('.zip')) continue;
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${file.name}</td>
+      <td>${formatFileSize(file.size)}</td>
+      <td>Waiting</td>
+      <td><progress value="0" max="100"></progress></td>
+      <td><button class="delete-button">X</button></td>
+    `;
+    fileTableBody.appendChild(row);
+    const deleteButton = row.querySelector('.delete-button');
+    deleteButton.addEventListener('click', () => {
+      fileTableBody.removeChild(row);
+      selectedFiles = selectedFiles.filter((selectedFile) => selectedFile !== file);
+      toggleFileTable();
+      updateStatusMessageOnFileChange();
+    });
+    selectedFiles.push(file);
+  }
+  toggleFileTable();
+  updateStatusMessageOnFileChange();
+});
+
+async function handleFileUpload(filesToUpload) {
   const uploadPromises = [];
 
-  for (const file of files) {
+  for (const file of filesToUpload) {
     const row = document.createElement('tr');
     row.innerHTML = `
       <td>${file.name}</td>
@@ -53,26 +83,21 @@ async function handleFileUpload() {
       <td><button class="delete-button">X</button></td>
     `;
     fileTableBody.appendChild(row);
-
     const uploadPromise = uploadFile(file, row)
       .then((status) => {
-        if (status === 200) {
-          uploadedFiles++;
-        } else {
-          failedFiles++;
-        }
+        if (status === 200) uploadedFiles++;
+        else failedFiles++;
       })
-      .catch((error) => {
-        console.error('Upload error:', error);
+      .catch(() => {
         row.querySelector('td:nth-child(3)').textContent = 'Failed';
         failedFiles++;
       });
-
     uploadPromises.push(uploadPromise);
-
     const deleteButton = row.querySelector('.delete-button');
     deleteButton.addEventListener('click', () => {
       fileTableBody.removeChild(row);
+      selectedFiles = selectedFiles.filter((selectedFile) => selectedFile !== file);
+      toggleFileTable();
     });
   }
 
@@ -86,47 +111,49 @@ async function handleFileUpload() {
   clearFileInput();
 }
 
-
 async function uploadFile(file, row) {
   const formData = new FormData();
   formData.append('file', file);
 
   try {
     const apiUrl = 'https://gcs-vvicnw7txq-uc.a.run.app/upload/';
-    
-    // Get the access token from localStorage
     const accessToken = localStorage.getItem('access_token');
-    
-    // Create headers with the access token
     const headers = new Headers({
       'Authorization': `Bearer ${accessToken}`,
     });
-    
     const response = await fetch(apiUrl, {
       method: 'POST',
       body: formData,
-      headers: headers, // Add headers to the request
+      headers: headers,
     });
 
     if (response.status === 200) {
-      // The upload was successful (status code 200)
       row.querySelector('td:nth-child(3)').textContent = 'Uploaded';
       row.querySelector('progress').value = 100;
-      return 200; // Return 200 for success
+      return 200;
     } else {
-      // The upload failed (status code other than 200)
       row.querySelector('td:nth-child(3)').textContent = 'Failed';
-      return response.status; // Return the actual status code for failure
+      return response.status;
     }
   } catch (error) {
     console.error('Upload error:', error);
     row.querySelector('td:nth-child(3)').textContent = 'Failed';
-    return 500; // Return a default status code for errors
+    return 500;
   }
 }
 
+function formatFileSize(bytes) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
 
-
+function clearFileTable() {
+  fileTableBody.innerHTML = '';
+  toggleFileTable();
+}
 
 function updateUploadStatusMessage(uploaded, failed) {
   const totalFiles = uploaded + failed;
@@ -138,60 +165,36 @@ function updateUploadStatusMessage(uploaded, failed) {
 
 function updateStatusMessageOnFileChange() {
   const selectedFilesCount = fileInput.files.length;
-  uploadStatusMessage.textContent = selectedFilesCount > 0
-    ? `Number of files selected: ${selectedFilesCount}. Press upload.`
+  uploadStatusMessage.textContent = selectedFiles.length > 0
+    ? `Number of files selected: ${selectedFiles.length}. Press upload.`
     : '';
 }
 
-// Your existing main page JavaScript code goes here (the code you provided earlier).
-
-// Check if a token is present in localStorage when the page loads
 document.addEventListener("DOMContentLoaded", function () {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-        // Token is present, do something with it (e.g., display authenticated content)
-        // For example, you can make authenticated API requests using the token.
-        // You can also redirect to a different page or perform other actions.
-
-        // Add an event listener to the logout button
-        const logOutButton = document.getElementById('logOut');
-        logOutButton.addEventListener('click', () => {
-            // Clear the token from localStorage
-            localStorage.removeItem('access_token');
-
-            // Redirect to the login page
-            window.location.href = 'login/login.html'; // Replace with the actual login page URL
-        });
-    } else {
-        // Token is not present, add an event listener to open the login page
-        const loginButton = document.getElementById('loginButton');
-        loginButton.addEventListener('click', () => {
-            // Open the login page in a new window or redirect to it
-            // You can customize this behavior based on your requirements
-            window.open('login/login.html', '_blank'); // Opens the login page in a new window
-            // Alternatively, you can use window.location.href to redirect
-        });
-    }
+  const token = localStorage.getItem('access_token');
+  if (token) {
+    const logOutButton = document.getElementById('logOut');
+    logOutButton.addEventListener('click', () => {
+      localStorage.removeItem('access_token');
+      window.location.href = 'login/login.html';
+    });
+  } else {
+    const loginButton = document.getElementById('loginButton');
+    loginButton.addEventListener('click', () => {
+      window.open('login/login.html', '_blank');
+    });
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Check if the access token is present in localStorage
-    const accessToken = localStorage.getItem('access_token');
-
-    if (accessToken) {
-        // Access token is present, add the event listener for logout
-        const logOutButton = document.getElementById("logOut");
-        logOutButton.addEventListener("click", function () {
-            // Remove the access token from localStorage
-            localStorage.removeItem('access_token');
-            // Redirect to the login page or perform any other actions you need
-            // For example, you can redirect to the login page:
-            window.location.href = 'login/login.html';
-        });
-    } else {
-        // Access token is not present, handle this scenario (e.g., show the login form)
-        // You can display the login form or perform other actions here
-        // For example, you can show a message or redirect to the login page:
-        window.location.href = 'login/login.html';
-    }
+  const accessToken = localStorage.getItem('access_token');
+  if (accessToken) {
+    const logOutButton = document.getElementById("logOut");
+    logOutButton.addEventListener("click", function () {
+      localStorage.removeItem('access_token');
+      window.location.href = 'login/login.html';
+    });
+  } else {
+    window.location.href = 'login/login.html';
+  }
 });
